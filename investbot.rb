f357f102b06846e7677f4c9c0c806515d43debb6
root <root@ip-172-31-48-187.ec2.internal>
2016-03-16 13:24:24 -0400

    fixed yaml file issues

--- investbot.rb
+++ investbot.rb
@@ -5,11 +5,10 @@ require 'nokogiri'
 require 'sequel'
 require 'json'
 require 'yaml'
+require 'twilio-ruby'
 
-#puts util
 
-# GET data from API
-
+stocks = ["KO","DOW","T","GE","GILD","SYK","JPM","UNH","PG","BA","HD","AAPL"]
 
 def getCurrentPrice(stockname)
 
@@ -41,6 +40,40 @@ def sendNotification(phone_number)
   })
 end
 
+def startProgram(stocks)
+
+  #read through config file to grab DB and API info
+  #config = YAML.load_file("config.yml")
+
+  #db_user = config['db_user']
+  db_user = 'investbot'
+  #db_pass = config['db_pass']
+  db_pass = 'investbot123'
+  #db_host = config['db_host']
+  db_host = 'investbotdb.c1p7w3xyq4hx.us-east-1.rds.amazonaws.com'
+  #db_name = config['db_name']
+  db_name = 'investbotdb'
+  #api_key = config['api_key']
+  # get connection to db - replace with connection to RDS
+  db = Sequel.connect(:adapter => 'mysql2', :user => db_user, :host => db_host, :database => db_name, :password => db_pass)
+
+  # get a handle to table
+  util = db[:stock_price]
+  
+  # iterate over data and insert records in table
+  stocks.each do |stock_name|
+    returned_value = getCurrentPrice(stock_name)
+    entry_time = Time.now
+     util.insert(
+       :stock_name => returned_value["Name"],
+       :stock_symbol => returned_value["Symbol"],
+       :timestamp => returned_value["Timestamp"],
+       :entry_time => entry_time,
+       :stock_price => returned_value["LastPrice"]
+     )
+    sleep(3)
+  end
+end
 
 #def getHistoricalValues(stockname, timeperiod, frequency, type)
 #
@@ -69,43 +102,6 @@ end
 #
 #end
 
+# Program Execution
 
-
-def startProgram(stocks)
-
-#read through config file to grab DB and API info
-#config = YAML.load_file("config.yml")
-
-#db_user = config['db_user']
-db_user = 'investbot'
-#db_pass = config['db_pass']
-db_pass = 'investbot123'
-#db_host = config['db_host']
-db_host = 'investbotdb.c1p7w3xyq4hx.us-east-1.rds.amazonaws.com'
-#db_name = config['db_name']
-db_name = 'investbotdb'
-#api_key = config['api_key']
-# get connection to db - replace with connection to RDS
-db = Sequel.connect(:adapter => 'mysql2', :user => db_user, :host => db_host, :database => db_name, :password => db_pass)
-
-# get a handle to table
-util = db[:stock_price]
-  # iterate over data and insert records in table
-  stocks.each do |stock_name|
-  # print "."
-  returned_value = getCurrentPrice(stock_name)
-  entry_time = Time.now
-   util.insert(
-     :stock_name => returned_value["Name"],
-     :stock_symbol => returned_value["Symbol"],
-     :timestamp => returned_value["Timestamp"],
-     :entry_time => entry_time,
-     :stock_price => returned_value["LastPrice"]
-   )
-  sleep(3)
-  end
-end
-
-
-stocks = ["KO","DOW","T","GE","GILD","SYK","JPM","UNH","PG","BA","HD","AAPL"]
 startProgram(stocks)
